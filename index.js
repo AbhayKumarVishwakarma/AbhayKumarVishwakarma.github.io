d728d9f89d647fbac769d14b43026bc066607d81
AbhayKumarVishwakarma <3492akv@gmail.com>
2023-05-11 10:33:24 +0530

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('resume-link-1').addEventListener("click", () => {
+    window.open('https://drive.google.com/file/d/10JrRdhqhGNTuAvklIAGXqTB4usqYRaPu/view?usp=share_link', '_blank', 'noopener, noreferrer');
+
+})
+
+document.getElementById('resume-link-2').addEventListener("click", () => {
+    // window.location.assign("https://drive.google.com/file/d/10JrRdhqhGNTuAvklIAGXqTB4usqYRaPu/view?usp=share_link", "_blank");
+    window.open('https://drive.google.com/file/d/10JrRdhqhGNTuAvklIAGXqTB4usqYRaPu/view?usp=share_link', '_blank', 'noopener, noreferrer');
+})
