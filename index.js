c20c365503083b6a80ad9a624d6e63d65bebab0d
AbhayKumarVishwakarma <3492akv@gmail.com>
2023-06-05 10:26:53 +0530

    done

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ document.getElementById('resume-link-1').addEventListener("click", () => {
 })
 
 document.getElementById('resume-link-2').addEventListener("click", () => {
-    // window.location.assign("https://drive.google.com/file/d/10JrRdhqhGNTuAvklIAGXqTB4usqYRaPu/view?usp=share_link", "_blank");
     window.open('https://drive.google.com/file/d/10JrRdhqhGNTuAvklIAGXqTB4usqYRaPu/view?usp=share_link', '_blank', 'noopener, noreferrer');
 })
+
+
+/* ----------------------- */
+
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link-container');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
